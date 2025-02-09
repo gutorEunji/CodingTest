6832a3d0b7c1fe37dbe3134eb689ec373171711a
unknown <vktmffl6790@gmail.com>
2025-02-09 16:46:33 +0900

    [JJ-02] BeakJook code update

---
+++ src/main/java/BaekJoon/Input.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+public class Input {
+
+    public void Q1000() {
+        System.out.println("Q1000");
+    }
+
+    public void Q1001() {
+        System.out.println("Q1001");
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import BaekJoon.Input;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Input input = new Input();
+
+        //input.Q1000();
+        input.Q1001();
     }
 }
